8758e59c7ae1e4254932e3f50520083b99d850e6
Hostlife22 <sima.serafim2021@mail.ru>
2022-03-07 15:36:26 +0300

    init: Slack-clone-app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCgWcmLh8Dugv1vk27cgGdf2c8f0k4zphc",
+  authDomain: "slack-clone-a81dc.firebaseapp.com",
+  projectId: "slack-clone-a81dc",
+  storageBucket: "slack-clone-a81dc.appspot.com",
+  messagingSenderId: "438583338981",
+  appId: "1:438583338981:web:d3c459bee673755045710c",
+};
